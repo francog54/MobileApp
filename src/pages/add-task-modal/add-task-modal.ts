d6952b158ba626eb527100ae4bbc28631b76e9ac
Franco <frankog_33@hotmail.com>
2017-07-07 15:51:32 -0300

    Evento para cerrar Vista Modal

--- src/pages/add-task-modal/add-task-modal.ts
+++ src/pages/add-task-modal/add-task-modal.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { ViewController, NavParams } from 'ionic-angular';
 
 /**
  * Generated class for the AddTaskModalPage page.
@@ -7,18 +7,22 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
  * See http://ionicframework.com/docs/components/#navigation for more info
  * on Ionic pages and navigation.
  */
-@IonicPage()
+
 @Component({
   selector: 'page-add-task-modal',
   templateUrl: 'add-task-modal.html',
 })
 export class AddTaskModalPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public viewCtrl: ViewController, public navParams: NavParams) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad AddTaskModalPage');
+  }
+
+  dismiss(){
+    this.viewCtrl.dismiss();
   }
 
 }
